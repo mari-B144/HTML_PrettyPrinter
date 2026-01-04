20c01e70805e5d9169c320272227fb737694757c
iooliah <toaderanaiulia@gmail.com>
2026-01-04 16:30:40 +0200

    Determinare identare

--- prettyhtml.sh
+++ prettyhtml.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-file=$1
+filename=$1
 
 if [ ! -f "$filename" ]; then
 	echo "$filename does not exist."
-else
-	text=$(cat "$filename")
-	echo "$text"
+	exit
 fi
+
+indent=0 
+while read line; do
+	if [[ "$line" == "<"* ]] && [[ "$line" != "</"* ]] && [[ "$line" != "<!"* ]] && [[ "$line" != *"/>" ]]; then
+		indent=$((indent + 1))
+		echo "$indent"
+	fi
+	if [[ "$line" == "</"* ]]; then
+		indent=$((indent - 1))
+		echo "$indent"
+	fi
+done < "$filename"
+
 
