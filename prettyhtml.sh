4daf829d404014376cab19e6be487e37e3a57855
iooliah <toaderanaiulia@gmail.com>
2026-01-08 23:26:28 +0200

    Taguri despartite corect

--- prettyhtml.sh
+++ prettyhtml.sh
@@ -3,27 +3,31 @@
 filename=$1
 
 if [ ! -f "$filename" ]; then
-	echo "$filename does not exist."
-	exit
+        echo "$filename does not exist."
+        exit
 fi
 
 indent=0
- 
+
+#s/</\n</g
+#s/>([^<[:space:]][^<]*)</>\n\1\n</g
+#s/([^[:space:]])(<[^>]+>)/\1\n\2\n/g
+##s/(>)([^<]+)/\1\n\2\n/g
+#s/([^>])(<)/\1\n\2/g 
 sed -E '
-s/></>\n</g
-s/>([^<[:space:]][^<]*)</>\n\1\n</g
-/^[[:space:]]*$/d
-' "$filename"
+s/(<[^>]+>)/\n\1\n/g
+' "$filename" | while read line; do
 
-while read line; do
-	if [[ "$line" == "<"* ]] && [[ "$line" != "</"* ]] && [[ "$line" != "<!"* ]] && [[ "$line" != *"/>" ]]; then
-		indent=$((indent + 1))
-		echo "$indent"
-	fi
-	if [[ "$line" == "</"* ]]; then
-		indent=$((indent - 1))
-		echo "$indent"
-	fi
-done < "$filename"
+        if [[ "$line" == "</"* ]]; then
+                indent=$((indent - 1))
+        fi
 
+	for ((i=0; i<indent; i++)); do
+    		echo -n "    "
+	done
+	echo "$line"
 
+        if [[ "$line" == "<"* ]] && [[ "$line" != "</"* ]] && [[ "$line" != "<!"* ]] && [[ "$line" != *"/>" ]]; then
+                indent=$((indent + 1))
+        fi
+done
