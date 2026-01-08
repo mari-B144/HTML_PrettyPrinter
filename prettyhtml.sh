9db244427c6e0c6490dbdb488892a323211c1de6
iooliah <toaderanaiulia@gmail.com>
2026-01-08 14:19:34 +0200

    Separare pe linii diferite

--- prettyhtml.sh
+++ prettyhtml.sh
@@ -7,7 +7,14 @@ if [ ! -f "$filename" ]; then
 	exit
 fi
 
-indent=0 
+indent=0
+ 
+sed -E '
+s/></>\n</g
+s/>([^<[:space:]][^<]*)</>\n\1\n</g
+/^[[:space:]]*$/d
+' "$filename"
+
 while read line; do
 	if [[ "$line" == "<"* ]] && [[ "$line" != "</"* ]] && [[ "$line" != "<!"* ]] && [[ "$line" != *"/>" ]]; then
 		indent=$((indent + 1))
